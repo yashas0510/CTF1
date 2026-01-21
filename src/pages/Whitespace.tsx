b4f7db397da1d7508da17a72cf219cb166af99b1
Yashas <byashas22comp@student.mes.ac.in>
2026-01-21 18:18:15 +0530

    new whitespace code

--- src/pages/Whitespace.tsx
+++ src/pages/Whitespace.tsx
@@ -1,44 +1,57 @@
 export default function Whitespace() {
-  // Binary → whitespace encoding
-  // 0 = space, 1 = tab
-  const hidden =
-    "01000110 01001001 01001110 01000001 01001100"
-      .replace(/0/g, " ")
-      .replace(/1/g, "\t")
+  /*
+    Encoding scheme:
+    U+200B (Zero Width Space) = 0
+    U+200C (Zero Width Non-Joiner) = 1
+
+    Binary for "ALL_SET":
+    A  01000001
+    L  01001100
+    L  01001100
+    _  01011111
+    S  01010011
+    E  01000101
+    T  01010100
+  */
+
+  const hidden = (
+    "Inspired\u200C\u200B\u200B\u200B\u200B\u200B\u200B\u200C\u200B " +
+    "by\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "fairytale\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "trolls,\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "King\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "conceived\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "of\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "a\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "shapeshifting\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "entity\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "living\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "in\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "the\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "sewers\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "that\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "could\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "embody\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "whatever\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "frightened\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "its\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "target\u200B\u200C\u200B\u200B\u200C\u200C\u200B\u200B " +
+    "most."
+  )
 
   return (
     <div className="center">
       <h2>The Last Illusion</h2>
 
-      <p style={{ maxWidth: "720px", lineHeight: "1.6" }}>
-        Inspired by fairytale trolls, King conceived of a shapeshifting entity
-        living in the sewers that could embody whatever frightened its target
-        most. Pennywise became the central form it used to lure children —
-        appearing to be a harmless clown. Scholars and critics have noted the
-        effectiveness of this design, as clowns are commonly viewed as uncanny.
+      <p style={{ maxWidth: "800px", lineHeight: "1.7" }}>
+        {hidden}
       </p>
 
-      <p style={{ marginTop: "20px", opacity: 0.8 }}>
-        Some messages are not meant to be seen.
-        <br />
-        They are meant to be <em>noticed</em>.
+      <p style={{ marginTop: "20px", opacity: 0.75 }}>
+        Not everything you copy is visible.
       </p>
 
-      <pre
-        style={{
-          marginTop: "30px",
-          background: "#000",
-          padding: "20px",
-          border: "1px solid #333",
-          whiteSpace: "pre"
-        }}
-      >
-{hidden}
-      </pre>
-
-      <p style={{ marginTop: "20px" }}>
-        You reached the end.
-      </p>
+      <p>You reached the end.</p>
     </div>
   )
 }
